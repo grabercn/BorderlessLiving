2092b06c1594d5842947d1d924fbbb83f3539bed
Christian G <cgrab713@gmail.com>
2025-02-20 20:38:47 +0000

    initial deployment of the website

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CountryHome from './CountryHome'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CountryHome />
+  </React.StrictMode>
+);
